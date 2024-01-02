bd7e5590291755c3830635785b0d7605c9e9b061
IvanGorbunov <falseprogrammerfirst@gmail.com>
2024-01-02 23:08:35 +0300

    fixed test_answer_page.py

--- src/functional_tests/test_answer_page.py
+++ src/functional_tests/test_answer_page.py
@@ -11,10 +11,10 @@ class TestProjectListPage(StaticLiveServerTestCase):
 
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--window-size=1420,1080')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
+        # chrome_options.add_argument('--no-sandbox')
+        # chrome_options.add_argument('--window-size=1420,1080')
+        # chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--disable-gpu')
         self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
     def tearDown(self):
