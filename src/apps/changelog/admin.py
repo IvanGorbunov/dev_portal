3f18bbe11e4d5ba60927581e9c8ed261aceae608
IvanGorbunov <falseprogrammerfirst@gmail.com>
2024-01-02 22:47:35 +0300

    fixed model`s history

--- src/apps/changelog/admin.py
+++ src/apps/changelog/admin.py
@@ -5,14 +5,14 @@ from .models import ChangeLog
 
 @admin.register(ChangeLog)
 class ChangeLogAdmin(admin.ModelAdmin):
-  list_display = (
-      'changed',
-      'model',
-      'user',
-      'record_id',
-      'data',
-      'ipaddress',
-      'action_on_model',
-  )
-  readonly_fields = ('user', )
-  list_filter = ('model', 'action_on_model',)
+    list_display = (
+        'changed',
+        'model',
+        'user',
+        'record_id',
+        'data',
+        'ipaddress',
+        'action_on_model',
+    )
+    readonly_fields = ('user',)
+    list_filter = ('model', 'action_on_model',)
